8f65a33fc86a68e0c1495d10918c55747beff974
Mateus Araújo <mateusaraujo996@gmail.com>
2021-08-15 14:36:17 -0300

    sticky navbar on scroll script and scroll-up button show/hide script

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
